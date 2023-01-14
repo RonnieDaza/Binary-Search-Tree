45b005d32acc75070368839ad8747f43b42a99c8
Ronnie M. Daza Jr <daza.ronnie.09212002@gmail.com>
2023-01-14 19:06:42 +0800

    Test the data

--- Binary Tree.py
+++ Binary Tree.py
@@ -8,3 +8,14 @@ class BinarySearchTreeNode:
         # Add data to left subtree
         if data == self.data:
             return
+        
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
